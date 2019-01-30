3520f41bf606290d3c23c63367aee60db737120a
William Serna <wserna@esquema.com>
2019-01-30 10:57:23 -0500

    Se crean metodo add, get, getall, update, delete en la libreria task. Se crean test para los metodos de la libreria task

--- src/lib/task/index.js
+++ src/lib/task/index.js
@@ -1,19 +1,62 @@
 'use strict'
 
 import Boom from 'boom'
+import Db from '../db'
+
+const db = Db.init({ dbName: 'db_taskio', collection: 'task' })
 
 class Task {
   constructor (id) {
     this.id = id
   }
 
-  get () {
+  static async add (data) {
+    try {
+      let res = await db.add(data)
+
+      return Promise.resolve(res)
+    } catch (error) {
+      return Promise.reject(new Boom(error))
+    }
+  }
+
+  static async getAll () {
+    try {
+      let res = await db.getAll()
+
+      return Promise.resolve(res)
+    } catch (error) {
+      return Promise.reject(new Boom(error))
+    }
+  }
+
+  async update (data) {
+    try {
+      let res = await db.update(this.id, data)
+
+      return Promise.resolve(res)
+    } catch (error) {
+      return Promise.reject(new Boom(error))
+    }
+  }
+
+  async get () {
     try {
       if (!this.id) throw Boom.notFound('Task id not found or invalid')
 
-      return Promise.resolve({
-        res: 200
-      })
+      let res = await db.get(this.id)
+
+      return Promise.resolve(res)
+    } catch (error) {
+      return Promise.reject(new Boom(error))
+    }
+  }
+
+  async delete () {
+    try {
+      let res = await db.delete(this.id)
+
+      return Promise.resolve(res)
     } catch (error) {
       return Promise.reject(new Boom(error))
     }
--- test/task.get.test.js
+++ test/task.get.test.js
@@ -3,21 +3,52 @@
 import test from 'ava'
 import { Task } from '../src'
 
+let newTask = {}
+
+test.before(async t => {
+  t.context.objTest = { name: 'Willy', email: 'wsernalaverde@gmail.com' }
+})
+
+test('Add task', async t => {
+  const taskData = await Task.add(t.context.objTest)
+  newTask = taskData
+  t.deepEqual(t.context.objTest.email, taskData.email)
+  t.truthy(taskData._id, true)
+})
+
 test('Get task data', async t => {
-  const task = new Task(2)
-
+  const task = new Task(newTask.id)
   const taskData = await task.get()
-
-  t.is(taskData.res, 200)
+  t.deepEqual(newTask.id, taskData.id)
 })
 
-test('Error task not found', async t => {
-  const task = new Task()
-
-  const taskData = await t.throwsAsync(() => {
-    return task.get()
-  })
-
-  t.regex(taskData.output.payload.message, /not found/)
-  t.deepEqual(taskData.output.statusCode, 404)
+test('Update task data', async t => {
+  const task = new Task(newTask.id)
+  newTask.lastname = 'Serna'
+  const taskData = await task.update(newTask)
+  t.deepEqual(taskData.lastname, newTask.lastname)
 })
+
+test('Delete task', async t => {
+  const task = new Task(newTask.id)
+  const taskData = await task.delete()
+  t.deepEqual(taskData.id, newTask.id)
+})
+
+// test('Get all tasks', async t => {
+//   const task = new Task(newTask.id)
+//   const taskData = await task.get()
+//   t.deepEqual(newTask.id, taskData.data.id)
+//   t.is(taskData.status_code, 200)
+// })
+
+// test('Error task not found', async t => {
+//   const task = new Task()
+
+//   const taskData = await t.throwsAsync(() => {
+//     return task.get()
+//   })
+
+//   t.regex(taskData.output.payload.message, /not found/)
+//   t.deepEqual(taskData.output.statusCode, 404)
+// })
